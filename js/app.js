3d1b35635f5f78ed27b97cbb289fa08261f2884f
Jorge Espinosa <joespla2000@gmail.com>
2018-05-15 19:06:58 -0500

    Dice img change with eventListener. Scores set to cero

--- js/app.js
+++ js/app.js
@@ -9,13 +9,28 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice;
 
 document.querySelector('.dice').style.display = 'none';
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+  // Random number
+  var dice = Math.floor(Math.random() * 6) + 1;
+
+  // Display result
+  var diceDOM = document.querySelector('.dice')
+  diceDOM.style.display = 'block';
+  diceDOM.src = '../img/dice-' + dice + '.png';
+
+  // Update round score IF the rolled number was not one
+});
+
+//document.querySelector('#current-' + activePlayer).textContent = dice;
