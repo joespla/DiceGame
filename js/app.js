75ec03dad3b551652ca492414a99d7f4ddf62ba7
Jorge Espinosa <joespla2000@gmail.com>
2018-05-15 20:26:03 -0500

    Hold button activated. Winner now is shown after 100 points

--- js/app.js
+++ js/app.js
@@ -24,26 +24,50 @@ document.getElementById('current-1').textContent = '0';
 document.querySelector('.btn-roll').addEventListener('click', function() {
   // Random number
   var dice = Math.floor(Math.random() * 6) + 1;
-
   // Display result
   var diceDOM = document.querySelector('.dice')
   diceDOM.style.display = 'block';
   diceDOM.src = '../img/dice-' + dice + '.png';
-
   // Update round score IF the rolled number was not one
   if (dice !== 1) {
     // Add score
     roundScore += dice;
     document.querySelector('#current-' + activePlayer).textContent = roundScore;
   }else {
-    document.getElementById('current-' + activePlayer).textContent = 0;
-    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
-    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
-    roundScore = 0;
-
-    diceDOM.style.display = 'none';
+    nextPlayer();
   }
 });
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+  // Add current score to global score
+  scores[activePlayer] += roundScore;
+  // Update the UI
+  document.getElementById('score-'+activePlayer).textContent = scores[activePlayer];
+  // Check if player won the game
+  if (scores[activePlayer] >= 10) {
+    document.getElementById('name-'+activePlayer).textContent = 'WINNER!';
+    document.querySelector('.dice').style.display = 'none';
+    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+  }else {
+    // Next player
+    nextPlayer();
+  }
+
+});
+
+function nextPlayer(){
+  // Set current score to zero
+  document.getElementById('current-' + activePlayer).textContent = 0;
+  roundScore = 0;
+  // Change class to activate based on the active player
+  document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+  // Change active player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  // Change class to activate based on the deactive player
+  document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+  // Hide dice for next player
+  document.querySelector('.dice').style.display = 'none';
+}
 
 //
