415e0d9b8174dea13af5383b37668030236c9971
Jorge Espinosa <joespla2000@gmail.com>
2018-05-15 19:47:13 -0500

    Active player and current score now changes when dice get 1

--- js/app.js
+++ js/app.js
@@ -31,6 +31,19 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
   diceDOM.src = '../img/dice-' + dice + '.png';
 
   // Update round score IF the rolled number was not one
+  if (dice !== 1) {
+    // Add score
+    roundScore += dice;
+    document.querySelector('#current-' + activePlayer).textContent = roundScore;
+  }else {
+    document.getElementById('current-' + activePlayer).textContent = 0;
+    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    document.querySelector('.player-' + activePlayer + '-panel').classList.toggle('active');
+    roundScore = 0;
+
+    diceDOM.style.display = 'none';
+  }
 });
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
+//
